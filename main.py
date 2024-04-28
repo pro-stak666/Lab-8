bba08612e620a1286079b8fe921ab9a8fd00ec6c
pro-stak666 <mineev_kirill2005@mail.ru>
2024-04-29 01:56:41 +0300

    лабораторная 8

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
+import cv2
 import time
 
-import cv2
 
-
-def image_processing():
-    img = cv2.imread('img_test.jpg')
-    #cv2.imshow('image', img)
+def task1():
+    img = cv2.imread('images/variant-6.png')
     w, h = img.shape[:2]
-    #(cX, cY) = (w // 2, h // 2)
-    #M = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
-    #rotated = cv2.warpAffine(img, M, (w, h))
-    #cv2.imshow('rotated', rotated)
-
-    #cat = img[250:580, 20:280]
-    #cv2.imshow('image', cat)
-
-    #r = cv2.selectROI(img)
-    #image_cropped = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-    #cv2.imshow('cropped', image_cropped)
-
-    cv2.line(img, (0, 0), (580, 600), (255, 0, 0), 5)
-    cv2.rectangle(img, (384, 10), (580, 128), (0, 252, 0), 3)
-    cv2.putText(img, 'Lab. No 8', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 3,
-                (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.imshow('img', img)
+    resized_img = cv2.resize(src=img, dsize=(w * 2, h * 2), fx=2, fy=2)
+    cv2.imshow('resized_img', resized_img)
 
 
-def video_processing():
-    cap = cv2.VideoCapture(1)
+def task2():
+    cap = cv2.VideoCapture("video.mp4")
     down_points = (640, 480)
     i = 0
+    counter_right = 0
+    counter_left = 0
+    last_pos = 0
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -41,15 +27,26 @@ def video_processing():
         ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
 
         contours, hierarchy = cv2.findContours(thresh,
-                            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                                               cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             if i % 5 == 0:
                 a = x + (w // 2)
                 b = y + (h // 2)
-                print(a, b)
+                if a <= 320 and last_pos != -1:
+                    last_pos = -1
+                    counter_left += 1
+                elif a > 320 and last_pos != 1:
+                    last_pos = 1
+                    counter_right += 1
+                print(a, last_pos)
+            cv2.putText(frame, f'{counter_left}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                        2)
+            cv2.putText(frame, f'{counter_right}', (590, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                        2)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -62,8 +59,8 @@ def video_processing():
 
 
 if __name__ == '__main__':
-    #image_processing()
-    video_processing()
+    # task1()
+    task2()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
